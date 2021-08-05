1c4294e0aeb884d462582c931997bd300c23bc3e
Victor Henrique Malta <victor.fobos@gmail.com>
2021-08-05 13:00:38 -0300

    Criando a classe PosicaoXadrez

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -8,24 +8,11 @@ namespace xadrex_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                //Tabuleiro de xadrez tem dimensão 8x8
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-                //Criando peças no tabuleiro através do método ColocarPeca(Peca p, Posicao pos)
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+            Console.WriteLine(pos);
 
-                //chama a classe que imprime o tabuleiro na tela
-                Tela.imprimirTabuleiro(tab);
-
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos.ToPosicao());
 
             Console.ReadKey();
         }
---
+++ xadrex-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{   //Classe dedicada à fazer as posições do tabuleiro de xadrez (A1 à H8)
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        //Método que converte a posição char+int em uma posição válida na matriz
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
