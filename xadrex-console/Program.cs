35dc590d5896e4a8e77691ebf5b965a3b291e9ce
Victor Henrique Malta <victor.fobos@gmail.com>
2021-08-05 13:20:56 -0300

    Melhorando a implementação do método ImprimirTabuleiro()

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -8,11 +8,28 @@ namespace xadrex_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            try
+            {
+                //Tabuleiro de xadrez tem dimensão 8x8
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos);
+                //Criando peças no tabuleiro através do método ColocarPeca(Peca p, Posicao pos)
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            Console.WriteLine(pos.ToPosicao());
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(6, 3));
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(5, 5));
+                tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(7, 1));
+
+                //chama a classe que imprime o tabuleiro na tela
+                Tela.ImprimirTabuleiro(tab);
+
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadKey();
         }
--- xadrex-console/Tela.cs
+++ xadrex-console/Tela.cs
@@ -6,13 +6,15 @@ namespace xadrex_console
     class Tela
     {
         //Classe feita para imprimir o tabuleiro na tela, criando de acordo com as propriedades indicadas de linhas e colunas.
-        public static void imprimirTabuleiro(Tabuleiro tab)
+        public static void ImprimirTabuleiro(Tabuleiro tab)
         {
             //Cria uma matriz de linhas e colunas que imprime os tracinhos e peças no tabuleiro
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
+                    
                     //Se não tiver peças na posição, imprime "-". Se houver, imprime a peça.
                     if (tab.Peca(i, j) == null)
                     {
@@ -20,11 +22,29 @@ namespace xadrex_console
                     }
                     else
                     {
-                        Console.Write(tab.Peca(i, j) + " ");
+                        ImprimirPeca(tab.Peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 //Ao terminar a linha, ocorre a quebra
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        //Método estático que diferencia e imprime as peças pretas como "Amarelas"
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
