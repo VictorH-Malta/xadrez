6e79d86f5323e2c1ebb404c0d8b1a0f6e497b17e
Victor Henrique Malta <victor.fobos@gmail.com>
2021-08-05 12:48:06 -0300

    Tratando erros com exceção personalizada

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -8,17 +8,26 @@ namespace xadrex_console
     {
         static void Main(string[] args)
         {
-            //Tabuleiro de xadrez tem dimensão 8x8
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                //Tabuleiro de xadrez tem dimensão 8x8
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            //Criando peças no tabuleiro através do método ColocarPeca()
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                //Criando peças no tabuleiro através do método ColocarPeca(Peca p, Posicao pos)
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            //chama a classe que imprime o tabuleiro na tela
-            Tela.imprimirTabuleiro(tab);
+                //chama a classe que imprime o tabuleiro na tela
+                Tela.imprimirTabuleiro(tab);
 
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
         }
     }
 }
--- xadrex-console/Tabuleiro/Tabuleiro.cs
+++ xadrex-console/Tabuleiro/Tabuleiro.cs
@@ -14,19 +14,54 @@
             Pecas = new Peca[linhas, colunas];
         }
 
-        //Este método foi criado para dar acesso à propriedade matriz Peca[,]
+        //Este método foi criado para dar acesso à propriedade matriz Peca[,] que é private
         public Peca Peca(int linha, int coluna)
         {
             return Pecas[linha, coluna];
         }
 
+        //Sobrecarga de método que recebe como parâmetro a posição
+        public Peca Peca(Posicao pos)
+        {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
+
+        //Método que confere existência de Peças no lugar
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
         //Método para incluir peça no tabuleiro
         public void ColocarPeca(Peca p, Posicao pos)
         {
+            if (ExistePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             //guarda a posição indicada e manda para o tabuleiro
             Pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
         }
 
+        //Implementando método que confere se a posição está dentro da matriz do tabuleiro 8x8
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //Método que gera excessão personalizada.
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
+        }
     }
 }
