6b05eed0fff0f1290198bddff52524de05fd3568
Victor Henrique Malta <victor.fobos@gmail.com>
2021-08-09 13:43:04 -0300

    Implementando as restrições de movimento

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -16,28 +16,39 @@ namespace xadrex_console
                 //Enquanto tiver rolando ainda a partida de xadrez...
                 while (!partida.Terminada)
                 {
-                    Console.Clear(); //Limpa o console
-                    //chama a classe que imprime o tabuleiro na tela
-                    Tela.ImprimirTabuleiro(partida.Tab);
+                    try
+                    {
+                        Console.Clear(); //Limpa o console
+                                         //chama a classe que imprime o tabuleiro na tela
+                        Tela.ImprimirTabuleiro(partida.Tab);
+                        Console.WriteLine();
+                        Console.WriteLine("Turno: " + partida.Turno);
+                        Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
 
-                    Console.WriteLine();
-                    Console.Write("Origem: ");
-                    //Lê uma posição e guarda em uma variável origem
-                    Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        //Lê uma posição e guarda em uma variável origem
+                        Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                        partida.ValidarPosicaoDeOrigem(origem);
 
-                    bool[,] posicoesPossiveis = partida.Tab.Peca(origem).MovimentosPossiveis();
-                    Console.Clear(); //Limpa o console
-                    //chama a classe que imprime o tabuleiro na tela
-                    Tela.ImprimirTabuleiro(partida.Tab, posicoesPossiveis);
+                        bool[,] posicoesPossiveis = partida.Tab.Peca(origem).MovimentosPossiveis();
+                        Console.Clear(); //Limpa o console
+                                         //chama a classe que imprime o tabuleiro na tela
+                        Tela.ImprimirTabuleiro(partida.Tab, posicoesPossiveis);
 
-                    Console.WriteLine();
-                    Console.Write("Destino: ");
-                    Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+                        partida.ValidarPosicaoDeDestino(origem, destino);
 
-                    partida.ExecutaMovimento(origem, destino);
+                        partida.RealizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
                 }
-
-                
 
             }
             catch (TabuleiroException e)
--- xadrex-console/Tabuleiro/Peca.cs
+++ xadrex-console/Tabuleiro/Peca.cs
@@ -15,6 +15,7 @@
             QteMovimentos = 0;
         }
 
+
         public void IncrementarQteMovimentos()
         {
             QteMovimentos++;
@@ -22,6 +23,31 @@
 
         //Criamos um método que retorna uma matriz booleana indicando os possíveis caminhos que uma peça pode executar
         //É um método abstrato porque é muito genérico e depende de cada peça. Logo, sua implementação se dará dentro das classes das peças
+
+        public bool ExisteMovimentosPossiveis()
+        {
+            //Recebe a matriz de movimentos possíveis
+            bool[,] mat = MovimentosPossiveis();
+            //Testa se existe ou não espaços na matriz para andar no tabuleiro
+            for (int i = 0; i < Tab.Linhas; i++)
+            {
+                for (int j = 0; j < Tab.Colunas; j++)
+                {
+                    if (mat[i, j] == true)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public bool PodeMoverPara(Posicao pos)
+        {
+            return MovimentosPossiveis()[pos.Linha, pos.Coluna];
+        }
+
         public abstract bool[,] MovimentosPossiveis();
         
 
