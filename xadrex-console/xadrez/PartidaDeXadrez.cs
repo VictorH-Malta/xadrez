c0e05638b5db8bf7f180051138c2a4354c527fe2
Victor Henrique Malta <victor.fobos@gmail.com>
2021-08-10 18:26:39 -0300

    Jogada especial promocao

--- xadrex-console/xadrez/PartidaDeXadrez.cs
+++ xadrex-console/xadrez/PartidaDeXadrez.cs
@@ -146,6 +146,19 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            // #Jogadaespecial Promocao
+            if ((p.Cor == Cor.Branca && destino.Linha == 0)||(p.Cor == Cor.Preta && destino.Linha == 7))
+            {
+                p = Tab.RetirarPeca(destino);
+                Pecas.Remove(p);
+                Peca dama = new Dama(Tab, p.Cor);
+                Tab.ColocarPeca(dama, destino);
+                Pecas.Add(dama);
+            }
+
+
             if (EstaEmCheque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -165,7 +178,7 @@ namespace xadrez
                 MudaJogador();
             }
 
-            Peca p = Tab.Peca(destino);
+            
 
             // #Jogadaespecial en passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
